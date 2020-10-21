c32080ca0391b692fe436e28556e83a0bb50ddb6
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-10-21 08:42:29 -0500

    made form

---
+++ src/Form.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Form = (props) => {
+	const [formData, setFormData] = React.useState(props.state);
+
+	const handleSubmit = (event) => {
+		event.preventDefault(); // Prevent Form from Refreshing
+		props.handleSubmit(formData); // Submit to Parents desired function
+		props.history.push('/'); //Push back to display page
+	};
+
+	const handleChange = (event) => {
+		setFormData({ ...formData, [event.target.name]: event.target.value });
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='text'
+				name='name'
+				value={formData.name}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='img'
+				value={formData.img}
+				onChange={handleChange}
+			/>
+			<input type='submit' value={props.label} />
+		</form>
+	);
+};
+
+export default Form;
